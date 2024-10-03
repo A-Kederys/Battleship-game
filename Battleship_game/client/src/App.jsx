0e81e68d8f293b15913f0556ff59fda037cc22ca
A-Kederys <your-email@example.com>
2024-10-03 19:01:52 +0300

    Set up socket connection

--- Battleship_game/client/src/App.jsx
+++ Battleship_game/client/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react';
 import axios from "axios";
+import io from 'socket.io-client';
 
 import styles from "./App.module.css";
 
+let socket;
+
 function App() {
-  const [count, setCount] = useState(0)
   const [array, setArray] = useState([]);
 
+  // fetch data from the API
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:8080/api");
     setArray(response.data.movies);
@@ -14,22 +17,33 @@ function App() {
   };
 
   useEffect(() => {
-    fetchAPI()
-  },[]);
+    socket = io("http://localhost:8080");
+
+    // socket connection listener
+    socket.on("connect", () => {
+      console.log(`Connected to server with ID: ${socket.id}`);
+    });
+
+    // when the component mounts
+    fetchAPI();
+
+    return () => {
+      // clean up the socket connection when the component unmounts
+      if (socket) socket.disconnect();
+    };
+  }, []);
 
   return (
     <>
       <div>
-        {array.map((movies, index) => (
+        {array.map((movie, index) => (
           <div className={styles.container} key={index}>
-            <p className={styles.movies}>{movies}</p>
+            <p className={styles.movies}>{movie}</p>
           </div>
         ))}
-        
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Battleship_game/server/server.js
+++ Battleship_game/server/server.js
@@ -1,22 +1,35 @@
-
 const express = require("express");
-const app = express();
-
-// fetching backend api, making sure backend accepts requests from frontend
-
+const http = require("http");
 const cors = require("cors");
-const corsOptions = {
-    origin: ["http://localhost:5173"],
-};
+const { Server } = require("socket.io");
 
-app.use(cors(corsOptions));
+const app = express();
+const server = http.createServer(app);
 
+const io = new Server(server, {
+    cors: {
+      origin: ["http://localhost:5173"],
+      methods: ["GET", "POST"],
+      credentials: true
+    }
+  })
 
-// backend api setup
+app.use(cors());
+
 app.get("/api", (req, res) => {
-    res.json({movies: ["Interstellar", "Back to The Future", "Frequency"] });
+  res.json({ movies: ["Interstellar", "Back to The Future", "Frequency"] });
 });
 
-app.listen(8080, () => {
-    console.log("Server started on port 8080");
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+
+  // handle events sent from client  
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
   });
+});
+
+server.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
